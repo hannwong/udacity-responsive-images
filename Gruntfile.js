432ff01f2f5b419d419ee7f1bcda4bdf49b2bb07
Jon Wong <jhannwong@gmail.com>
2017-01-27 07:40:50 +0800

    Aligns Gruntfile.js
    Official solution cuts even its own part 2.
    
    Added back admin structure I need.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,11 @@
+/*
+ After you have changed the settings at "Your code goes here",
+ run this with one of these options:
+  "grunt" alone creates a new, completed images directory
+  "grunt clean" removes the images directory
+  "grunt responsive_images" re-processes images without removing the old ones
+*/
+
 module.exports = function(grunt) {
 
   grunt.initConfig({
@@ -11,6 +19,11 @@ module.exports = function(grunt) {
             quality: 50
           }]
         },
+
+        /*
+        You don't need to change this part if you don't change
+        the directory structure.
+        */
         files: [{
           expand: true,
           src: ['*.{gif,jpg,png}'],
@@ -19,9 +32,53 @@ module.exports = function(grunt) {
         }]
       }
     },
+
+    imageoptim: {
+      dev: {
+        options: {
+          jpegMini: false,
+          imageAlpha: false,
+          quitAfter: true
+        },
+        src: ['images']
+      }
+    },
+
+    /* Clear out the images directory if it exists */
+    clean: {
+      dev: {
+        src: ['images'],
+      },
+    },
+
+    /* Generate the images directory if it is missing */
+    mkdir: {
+      dev: {
+        options: {
+          create: ['images']
+        },
+      },
+    },
+
+    /* Copy the "fixed" images that don't go through processing into the images/directory */
+    copy: {
+      dev: {
+        files: [{
+          expand: true,
+          cwd: 'images_src', /* Corrects Udacity code, avoids nested folder. */
+          src: 'fixed/*.{gif,jpg,png}',
+          dest: 'images/'
+        }]
+      },
+    },
   });
 
   grunt.loadNpmTasks('grunt-responsive-images');
-  grunt.registerTask('default', ['responsive_images']);
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-mkdir');
+  grunt.loadNpmTasks('grunt-imageoptim');
+  grunt.registerTask('default', ['clean', 'mkdir', 'copy', 'responsive_images']);
+  grunt.registerTask('optim-img', ['imageoptim']);
 
 };
